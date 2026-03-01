a81f0bda7c9d463cea33063314f05dcd9ee54282
chriz <josechris042@gmail.com>
2026-03-02 01:50:44 +0400

    fhs environment

--- configuration.nix
+++ configuration.nix
@@ -232,6 +232,7 @@
 
   # rofi-power-menu and packages from nixpkgs
   environment.systemPackages = with pkgs; [
+
   (pkgs.writeShellScriptBin "rofi-power" ''
     ${pkgs.rofi}/bin/rofi -show power -modi "power:${pkgs.bash}/bin/bash /home/chriz/.config/rofi/scripts/rofi-power-menu" -theme-str 'window { width: 11%; }'
   '')
@@ -257,6 +258,27 @@
       cp -r * $out/share/icons/
     '';
   })
+
+  # Create an FHS environment using the command `fhs`, enabling the execution of non-NixOS packages in NixOS!
+    (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
+      pkgs.buildFHSEnv (base // {
+      name = "fhs";
+      targetPkgs = pkgs:
+        # pkgs.buildFHSEnv provides only a minimal FHS environment,
+        # lacking many basic packages needed by most software.
+        # Therefore, we need to add them manually.
+        
+        # pkgs.appimageTools provides basic packages required by most software.
+        (base.targetPkgs pkgs) ++ (with pkgs; [
+          pkg-config
+          ncurses
+          # Feel free to add more packages here if needed.
+        ]
+      );
+      profile = "export FHS=1";
+      runScript = "bash";
+      extraOutputsToInstall = ["dev"];
+    }))
 
   xdg-desktop-portal-gnome
   xdg-desktop-portal-gtk
