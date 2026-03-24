837075248e3f27b7fba590c5f2862503eaae45d0
chriz <josechris042@gmail.com>
2026-03-24 19:13:16 +0400

    install.sh 4

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ### ❄️ chriztinkrz's nixos config
-run ```bash
-curl -L https://raw.githubusercontent.com/chriztinkrz/nixos-config/main/install.sh | bash ``` for installation
+``` curl -L https://raw.githubusercontent.com/chriztinkrz/nixos-config/main/install.sh | bash ```
--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ nix-shell -p git --run "git clone https://github.com/chriztinkrz/wallpapers.git 
 cd ~/nixos-config
 mkdir -p configs
 sudo nixos-generate-config --show-hardware-config > ./configs/hardware-configuration.nix
+git add -f configs/hardware-configuration.nix
 git add .
 sudo nixos-rebuild switch --flake .#nixosbtw
+swww-daemon &
+sleep 1
+swww img /home/chriz/Pictures/Pictures/wallpapers/fluid2.jpg --transition-type grow --transition-duration 1.75
+ln -sf /home/chriz/Pictures/Pictures/wallpapers/fluid2.jpg "$HOME/.cache/current_wallpaper.png"
+hellwal -i /home/chriz/Pictures/Pictures/wallpapers/fluid2.jpg
