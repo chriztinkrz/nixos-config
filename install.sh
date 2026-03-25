6722c73e0830eea5917005ba33a1039e440ccd52
chriz <josechris042@gmail.com>
2026-03-25 11:12:27 +0400

    install.sh 8

--- install.sh
+++ install.sh
@@ -2,14 +2,10 @@ mkdir -p ~/Pictures/Pictures/wallpapers
 nix-shell -p git --run "git clone https://github.com/chriztinkrz/nixos-config.git ~/nixos-config"
 nix-shell -p git --run "git clone https://github.com/chriztinkrz/wallpapers.git ~/Pictures/Pictures/wallpapers"
 cd ~/nixos-config
-mkdir -p configs
 sudo nixos-generate-config --show-hardware-config > ./configs/hardware-configuration.nix
-git add -f configs/hardware-configuration.nix
 git add .
 sudo nixos-rebuild switch --flake .#nixosbtw
 nix-shell -p swww hellwal imagemagick --run "
-  ~/nixos-config/home-manager/hypr/scripts/install_wallpaper_cache.sh
-  sleep 1
   ln -sf ~/Pictures/Pictures/wallpapers/fluid2.jpg $HOME/.cache/current_wallpaper.png
   hellwal -i ~/Pictures/Pictures/wallpapers/fluid2.jpg
 "
