fec47b0cfaab18f93f2db378b68aeede4976fdc3
chriz <josechris042@gmail.com>
2026-04-14 15:27:15 +0400

    formatting

--- configs/system.nix
+++ configs/system.nix
@@ -65,7 +65,7 @@
   # set your time zone.
   time.timeZone = "Asia/Muscat";
 
-  # internationalisation properties.
+  # internationalisation properties
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
