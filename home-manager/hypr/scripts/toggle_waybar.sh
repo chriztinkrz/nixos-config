65b8370d9200e25f4d0f6f8d9dd76187da8466b1
chriztinkrz <josechris042@gmail.com>
2026-04-18 18:45:19 +0400

    no waybar toggle

--- home-manager/hypr/scripts/toggle_waybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if pgrep "waybar" > /dev/null
-then
-    pkill waybar
-    hyprctl keyword general:gaps_out 16 16 16 16
-else
-    waybar &
-    hyprctl keyword general:gaps_out 11 16 16 16
-fi
