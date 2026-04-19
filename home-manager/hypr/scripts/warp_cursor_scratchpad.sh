b62957f43f1f03e8898a343050db575eeb3f327d
chriz <josechris042@gmail.com>
2026-04-19 21:46:01 +0400

    warp cursor to scratchpad window

---
+++ home-manager/hypr/scripts/warp_cursor_scratchpad.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+hyprctl dispatch togglespecialworkspace magic
+sleep 0.05
+hyprctl dispatch movecursor $(hyprctl activewindow -j | jq -r '
+  (.at[0] + .size[0]/2 | floor | tostring) + " " +
+  (.at[1] + .size[1]/2 | floor | tostring)
+')
