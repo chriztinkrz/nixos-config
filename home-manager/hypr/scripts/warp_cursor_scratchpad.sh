56073a5090b6b20a167acfe61a6845508b4bb65c
chriz <josechris042@gmail.com>
2026-04-19 22:33:43 +0400

    no delay in warp cursor to scratchpad window

--- home-manager/hypr/scripts/warp_cursor_scratchpad.sh
+++ home-manager/hypr/scripts/warp_cursor_scratchpad.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 hyprctl dispatch togglespecialworkspace magic
-sleep 0.05
+# sleep 0.02
 hyprctl dispatch movecursor $(hyprctl activewindow -j | jq -r '
   (.at[0] + .size[0]/2 | floor | tostring) + " " +
   (.at[1] + .size[1]/2 | floor | tostring)
