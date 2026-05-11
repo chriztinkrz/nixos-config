4b2926af9ab17cf7c29abf84caa74a66c71f6ac4
chriztinkrz <josechris042@gmail.com>
2026-05-11 19:21:44 +0400

    update-config 2

--- home-manager/fish/functions/update-config.fish
+++ home-manager/fish/functions/update-config.fish
@@ -1,3 +1,7 @@
-function update-config --wraps='cd ~/Pictures/Pictures/wallpapers/ && git fetch origin && git reset --hard origin/main && cd ~/nixos-config && git fetch origin && git reset --hard origin/main' --description 'alias update-config=cd ~/Pictures/Pictures/wallpapers/ && git fetch origin && git reset --hard origin/main && cd ~/nixos-config && git fetch origin && git reset --hard origin/main'
-    cd ~/Pictures/Pictures/wallpapers/ && git fetch origin && git reset --hard origin/main && cd ~/nixos-config && git fetch origin && git reset --hard origin/main $argv
+function update-config --wraps='cd ~/nixos-config && git fetch origin && git reset --hard origin/main && cd ~/Pictures/Pictures/wallpapers/ && git fetch origin && git reset --hard origin/main' --description 'update nixos-config, wallpapers after'
+    cd ~/nixos-config && git fetch origin && git reset --hard origin/main; or return 1
+
+    cd ~/Pictures/Pictures/wallpapers/ && git fetch origin && git reset --hard origin/main; or return 1
+
+    cd - $argv
 end
