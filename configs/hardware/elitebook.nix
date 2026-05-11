eeaba4e016b6d2f534682cbeb5018cf9be34bc5e
chriz <josechris042@gmail.com>
2026-05-11 22:32:37 +0400

    plymouth but not for eb & vcinae colors

--- configs/hardware/elitebook.nix
+++ configs/hardware/elitebook.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+ { config, pkgs, lib, ... }:
 {
-  boot.kernelModules = [ "amdgpu" ];
+  boot = {
+    kernelModules = [ "amdgpu" ];
+    plymouth.enable = lib.mkForce false;
+    kernelParams = [];
+    consoleLogLevel = lib.mkForce 4;
+  };
 }
