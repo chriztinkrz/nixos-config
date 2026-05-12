2b398ea1f6dcdbb3f2549f026457b63a8f779789
chriztinkrz <josechris042@gmail.com>
2026-05-12 20:16:39 +0400

    back to nixpkgs hyprland

--- configs/packages.nix
+++ configs/packages.nix
@@ -9,8 +9,8 @@
   programs.foot.enable = true;
   programs.hyprland= {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    /*package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; */
   };
   programs.hyprlock.enable = true;
   programs.waybar.enable = true;
