c543ab337161eedd0f32d09adfdf6321ba0a613c
chriz <josechris042@gmail.com>
2026-05-20 21:08:52 +0400

    fire_village wp added

--- home-manager/hypr/scripts/center_cursor.lua
+++ home-manager/hypr/scripts/center_cursor.lua
@@ -2,8 +2,6 @@ local function center_cursor_on_window(win)
     if win and win.at and win.size then
         local center_x = math.floor(win.at.x + (win.size.x / 2))
         local center_y = math.floor(win.at.y + (win.size.y / 2))
-
-        -- Warp cursor using structural dispatcher
         hl.dispatch(hl.dsp.cursor.move({ x = center_x, y = center_y }))
     end
 end
