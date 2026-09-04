ab873e7d5fd75d4d36b1544ecfb000118cd746d8
chriztinkrz <josechris042@gmail.com>
2026-09-04 19:17:38 +0400

    rtl8821ce drivers on 3018tu

--- configs/hardware/3018tu.nix
+++ configs/hardware/3018tu.nix
@@ -1,6 +1,9 @@
-{ inputs, pkgs, config, ... }: {
+{ inputs, pkgs, config, lib, ... }: {
   networking.networkmanager.wifi.powersave = false;
   boot.blacklistedKernelModules = [ "rtw88_8821ce" "rtw88_pci" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8821ce ];
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_18;
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    (callPackage ./rtl8821ce.nix {  })
+  ];
   boot.kernelModules = [ "8821ce" ];
 }
---
+++ configs/hardware/rtl8821ce.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchpatch,
+  kernel,
+  kernelModuleMakeFlags,
+  bc,
+  nix-update-script,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rtl8821ce";
+  version = "0-unstable-2025-10-13";
+
+  src = fetchFromGitHub {
+    owner = "tomaspinho";
+    repo = "rtl8821ce";
+    rev = "4e6b887f0d8c4091a4df9da9fcead9a8294b41ad";
+    hash = "sha256-fY0j6VzwAIsD62+snAWfIgGXcwne0mOwIE/Yh25lwTY=";
+  };
+
+  hardeningDisable = [ "pic" ];
+
+  nativeBuildInputs = [ bc ] ++ kernel.moduleBuildDependencies;
+  makeFlags = kernelModuleMakeFlags;
+
+  prePatch = ''
+    substituteInPlace ./Makefile \
+      --replace-fail /lib/modules/ "${kernel.dev}/lib/modules/" \
+      --replace-fail /sbin/depmod \# \
+      --replace-fail '$(MODDESTDIR)' "$out/lib/modules/${kernel.modDirVersion}/kernel/net/wireless/"
+  '';
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/tomaspinho/rtl8821ce/pull/378.patch";
+      hash = "sha256-dnifyMew9PMZkVG4B4kZmyjVp+5BH/XBiu5yS+JrDqE=";
+    })
+  ];
+
+  preInstall = ''
+    mkdir -p "$out/lib/modules/${kernel.modDirVersion}/kernel/net/wireless/"
+  '';
+
+  enableParallelBuilding = true;
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch=master" ]; };
+
+  meta = {
+    description = "Realtek rtl8821ce driver";
+    homepage = "https://github.com/tomaspinho/rtl8821ce";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ defelo ];
+    broken = stdenv.hostPlatform.isAarch64;
+  };
+})
